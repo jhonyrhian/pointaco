d36fe38ef65848c43add7b8a9e03cf72b7fb1f3b
jhonyrhian <jhonyamandio@gmail.com>
2021-12-24 00:06:10 -0300

    commit inicial

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+function dablank(){
+    document.getElementById("blank").style.visibility = "visible";
+    document.getElementById("blank").style.backgroundColor = "rgba(255,255,255,0.6)"
+}
+
+function noblank(){
+    document.getElementById("blank").style.visibility = "hidden";
+    document.getElementById("blank").style.backgroundColor = "rgba(255,255,255,0)"
+}
